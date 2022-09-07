7a9dd7a2e93d31a31201ae89fd3419fa4c5825f7
Eganin <egorzaharin12@gmail.com>
2022-09-07 15:00:59 +0500

    add progress bar and text error for main page

--- app/src/main/java/com/eganin/jetpack/thebest/weatherapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/eganin/jetpack/thebest/weatherapp/presentation/ui/MainActivity.kt
@@ -8,13 +8,15 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.eganin.jetpack.thebest.weatherapp.presentation.ui.theme.DarkBlue
 import com.eganin.jetpack.thebest.weatherapp.presentation.ui.theme.DeepBlue
@@ -47,17 +49,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(DarkBlue)
-                    ) {
-                        WeatherCard(
-                            state = viewModel.state,
-                            backgroundColor = DeepBlue
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        WeatherForecast(state = viewModel.state)
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(DarkBlue)
+                        ) {
+                            WeatherCard(
+                                state = viewModel.state,
+                                backgroundColor = DeepBlue
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            WeatherForecast(state = viewModel.state)
+                        }
+                        if (viewModel.state.isLoading) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                        viewModel.state.error?.let { error ->
+                            Text(
+                                text = error,
+                                color = Color.Red,
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
                     }
                 }
             }
