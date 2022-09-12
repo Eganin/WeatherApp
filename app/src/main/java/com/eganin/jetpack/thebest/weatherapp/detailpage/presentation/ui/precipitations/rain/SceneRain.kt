36aadf53041d4fef530bef25090aa8d17503b498
Eganin <egorzaharin12@gmail.com>
2022-09-12 12:46:11 +0500

    add models for rain

---
+++ app/src/main/java/com/eganin/jetpack/thebest/weatherapp/detailpage/presentation/ui/precipitations/rain/SceneRain.kt
@@ -0,0 +1,4 @@
+package com.eganin.jetpack.thebest.weatherapp.detailpage.presentation.ui.precipitations.rain
+
+class SceneRain {
+}
--- app/src/main/java/com/eganin/jetpack/thebest/weatherapp/weeklist/ui/WeekListPage.kt
+++ app/src/main/java/com/eganin/jetpack/thebest/weatherapp/weeklist/ui/WeekListPage.kt
@@ -1,6 +1,5 @@
 package com.eganin.jetpack.thebest.weatherapp.weeklist.ui
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
